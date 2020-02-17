5c066d60b9ea0dd0da11d00a539da49b14317887
irohafox <matsukawa@grabss.co.jp>
2020-02-17 14:40:37 +0900

    saml controller/view&routes

---
+++ app/controllers/saml_idp_controller.rb
@@ -0,0 +1,40 @@
+class SamlIdpController < SamlIdp::IdpController
+  def new
+    super
+  end
+
+  def show
+    super
+  end
+
+  def create
+    super
+  end
+
+  def logout
+    super
+  end
+
+  # 以下は公式サンプル(https://github.com/saml-idp/saml_idp)
+  def idp_authenticate(email, password) # not using params intentionally
+    user = User.by_email(email).first
+    user && user.valid_password?(password) ? user : nil
+  end
+  private :idp_authenticate
+
+  def idp_make_saml_response(found_user) # not using params intentionally
+    # NOTE encryption is optional
+    encode_response found_user, encryption: {
+      cert: saml_request.service_provider.cert,
+      block_encryption: 'aes256-cbc',
+      key_transport: 'rsa-oaep-mgf1p'
+    }
+  end
+  private :idp_make_saml_response
+
+  def idp_logout
+    user = User.by_email(saml_request.name_id)
+    user.logout
+  end
+  private :idp_logout
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root to: 'root#index'
 
   devise_for :users
+
+  get '/saml/auth', to: 'saml_idp#new'
+  get '/saml/metadata', to: 'saml_idp#show'
+  post '/saml/auth', to: 'saml_idp#create'
+  match '/saml/logout', to: 'saml_idp#logout', via: [:get, :post, :delete]
 end
